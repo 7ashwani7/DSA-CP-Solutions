31c25a090a9d5b398a521ad44ccfd6cdc0892ec6
7ashwani7 <chauhanashwani548@gmail.com>
2026-07-30 21:03:49 +0530

    .

---
+++ GFG/LCM_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int gcd(int a , int b){
+        if(b == 0) return a;
+        return gcd(b , a % b);
+    }
+    int lcm(int a, int b) {
+        // code here
+        return (a / gcd(a , b)) * b;
+    }
+};
+int main() {
+    return 0;
+}
