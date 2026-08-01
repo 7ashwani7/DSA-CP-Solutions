7a9ae8b86fbdfe758c15a335125c88b1f99bfaff
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-01 21:19:16 +0530

    .

--- LeetCode/Ques24.cpp
+++ LeetCode/Ques24.cpp
@@ -1,13 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <bits/stdc++.h>
+using namespace std;
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
@@ -59,3 +59,6 @@ public:
         return head;
     }
 };
+int main() {
+    return 0;
+}
