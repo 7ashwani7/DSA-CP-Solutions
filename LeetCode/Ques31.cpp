ca038b83605f962af483d5f1ee3cb8e0cd55fb8a
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-04 18:04:29 +0530

    .

--- LeetCode/Ques31.cpp
+++ LeetCode/Ques31.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
@@ -29,3 +31,6 @@ public:
         return;
     }
 };
+int main() {
+    return 0;
+}
