39ca0a81aed0d2929bf1eef812650c988de5d0ac
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-08 19:06:09 +0530

    .

--- LeetCode/Ques38.cpp
+++ LeetCode/Ques38.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     string countAndSay(int n) {
@@ -19,3 +21,6 @@ public:
           return ztr;
     }
 };
+int main() {
+    return 0;
+}
