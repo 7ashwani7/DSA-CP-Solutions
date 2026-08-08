7991bc132be91bbeee16cfbfbc82c504583e4fe4
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-08 19:04:16 +0530

    .

--- GFG/Even_and_Odd.cpp
+++ GFG/Even_and_Odd.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     void reArrange(int arr[], int n) {
@@ -18,3 +20,6 @@ public:
         }
     }
 };
+int main() {
+    return 0;
+}
