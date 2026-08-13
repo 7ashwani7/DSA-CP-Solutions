e21a5873399bd9ac7d7116e5ce27db8618f561db
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-13 21:22:35 +0530

    .

---
+++ LeetCode/Ques46.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public: 
+    vector<vector<int>> ans;
+    void f(vector<int>& nums, int i){
+        if(i == nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+       // unordered_set<int> s;
+        for(int idx = i; idx < nums.size(); idx++){
+           // if(s.count(nums[idx])) continue;
+            //s.insert(nums[idx]);
+            swap(nums[idx] , nums[i]);
+            f(nums , i+1);
+            swap(nums[idx] , nums[i]);
+
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        f(nums, 0);
+        return ans;
+        
+    }
+};
+int main() {
+    return 0;
+}
