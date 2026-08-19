f6aad7d9a1ec0b0754b23407a8cdb60ee1896440
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-19 21:53:57 +0530

    .

---
+++ Codeforces/String_Task.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+
+    string ans = "";
+
+    for (char c : s) {
+        c = tolower(c);
+        if (c == 'a' || c == 'o' || c == 'y' || c == 'e' || c == 'u' || c == 'i') {
+            continue;
+        }
+        ans += '.';
+        ans += c;
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
