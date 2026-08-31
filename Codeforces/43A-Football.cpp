69fa652524996bd24124cb7ed08257f3f958d6ab
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-31 19:09:35 +0530

    .

--- Codeforces/43A-Football.cpp
+++ Codeforces/43A-Football.cpp
@@ -4,27 +4,20 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
     unordered_map<string, int> mp;
-
     string team;
-
     for (int i = 0; i < n; i++) {
         cin >> team;
         mp[team]++;
     }
-
     string ans;
     int mx = 0;
-
     for (auto x : mp) {
         if (x.second > mx) {
             mx = x.second;
             ans = x.first;
         }
     }
-
-    cout << ans;
-
+    cout << ans << endl;
     return 0;
 }
