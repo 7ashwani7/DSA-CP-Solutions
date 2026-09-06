0610e5c002b4dcb1d19951973eb9bdb8e16603a4
7ashwani7 <chauhanashwani548@gmail.com>
2026-09-06 21:06:56 +0530

    .

---
+++ GFG/Array_Traversal.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+void arrayTraversal(int arr[], int size) {
+    // Code here
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main() {
+    return 0;
+}
---
+++ GFG/Odd_or_Even.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    bool isEven(int n) {
+        // code here
+        if(n % 2 == 0) return true;
+        else return false;
+    }
+};
+int main() {
+    return 0;
+}
