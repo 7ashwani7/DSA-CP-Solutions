51ee0e10fe76d648571ba765e7a94a1bdb832f1f
7ashwani7 <chauhanashwani548@gmail.com>
2026-09-15 23:23:42 +0530

    .

--- Codeforces/59A-Word.cpp
+++ Codeforces/59A-Word.cpp
@@ -3,18 +3,16 @@ using namespace std;
 
 int main() {
     string s;
-    cin>>s;
+    cin >> s;
     int upper = 0;
     for (char c : s) {
         if (isupper(c)) upper++;
     }
     if (upper > s.size() - upper) {
         for (char &c : s) c = toupper(c);
-    } 
-    else {
+    } else {
         for (char &c : s) c = tolower(c);
     }
-    cout<<s<< endl;
-
+    cout << s;
     return 0;
 }
