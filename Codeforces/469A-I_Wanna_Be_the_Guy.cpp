c69f741d4b35b4d8b4d24bb3372e1f698605025e
7ashwani7 <chauhanashwani548@gmail.com>
2026-09-17 18:23:30 +0530

    .

---
+++ Codeforces/469A-I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<bool> v(n + 1, false);
+    int p;
+    cin >> p;
+    for (int i = 0; i < p; i++) {
+        int x;
+        cin >> x;
+        v[x] = true;
+    }
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int x;
+        cin >> x;
+        v[x] = true;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!v[i]) {
+            cout << "Oh, my keyboard!";
+            return 0;
+        }
+    }
+    cout << "I become the guy!";
+    return 0;
+}
