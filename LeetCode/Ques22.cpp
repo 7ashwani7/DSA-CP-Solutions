68b8f77d1806f5ef0081fbc575e23217bc306d9e
7ashwani7 <chauhanashwani548@gmail.com>
2026-09-22 09:16:44 +0530

    .

--- LeetCode/Ques22.cpp
+++ LeetCode/Ques22.cpp
@@ -17,6 +17,33 @@ public:
        return ans; 
     }
 };
+// OR
+class Solution {
+public:
+    vector<string> ans;
+    void generate(string& s, int open, int close, int n) {
+        if (close == n) {
+            ans.push_back(s);
+            return;
+        }
+        if (open < n) {
+            s.push_back('(');
+            generate(s, open + 1, close, n);
+            s.pop_back();
+        }
+        if (close < open) {
+            s.push_back(')');
+            generate(s, open, close + 1, n);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        ans.clear();
+        string s;
+        generate(s, 0, 0, n);
+        return ans;
+    }
+};
 int main() {
     return 0;
 }
--- LeetCode/Ques78.cpp
+++ LeetCode/Ques78.cpp
@@ -19,6 +19,26 @@ public:
         
     }
 };
+// OR 
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void helper(vector<int>& nums, vector<int>& curr,int idx) {
+        if (idx == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        curr.push_back(nums[idx]);
+        helper(nums, curr, idx + 1);
+        curr.pop_back();
+        helper(nums, curr, idx + 1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr;
+        helper(nums, curr, 0);
+        return ans;
+    }
+};
 int main() {
     return 0;
 }
