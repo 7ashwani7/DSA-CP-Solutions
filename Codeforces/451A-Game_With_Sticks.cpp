52f617b87833aed76db8d5d1cf63aff0cf9ec444
7ashwani7 <chauhanashwani548@gmail.com>
2026-09-23 16:20:32 +0530

    .

---
+++ Codeforces/451A-Game_With_Sticks.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (min(n, m) % 2 == 1) cout << "Akshat" << endl;
+    else cout << "Malvika" << endl;
+    return 0;
+}
