ebcbf0418a4b3582dfb1884b2eb62aa87c9f711e
7ashwani7 <chauhanashwani548@gmail.com>
2026-09-24 22:17:34 +0530

    .

---
+++ LeetCode/Ques3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int left = 0;
+        int ans = 0;
+        vector<int> last(256, -1);
+        for(int right = 0; right < n; right++){
+            if(last[s[right]] >= left) left = last[s[right]] + 1;
+            last[s[right]] = right;
+            ans = max(ans, right-left+1);
+        }
+        return ans;
+    }
+};
+int main() {
+    return 0;
+}
