8d5cc2437319c48539b5b5c2d5157d0902766fad
7ashwani7 <chauhanashwani548@gmail.com>
2026-09-24 22:22:08 +0530

    .

---
+++ LeetCode/Ques424.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        vector<int> freq(26, 0);
+        int l = 0;
+        int maxFreq = 0;
+        int ans = 0;
+        for(int r = 0; r < n; r++){
+            freq[s[r] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[r] - 'A']);
+            while((r - l + 1) - maxFreq > k){
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
